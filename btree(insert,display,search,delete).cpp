93feb070e3e4e2ea5c97fceec9f1ff7de36fabbe
Praneet7799 <31165495+Praneet7799@users.noreply.github.com>
2018-04-05 10:24:43 +0000

    Update btree(insert,display,search,delete).cpp
    fixed minor errors

--- btree(insert,display,search,delete).cpp
+++ btree(insert,display,search,delete).cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-//class to create node
+
 class node{
      public:
      node *parent;
@@ -13,38 +13,37 @@ class node{
     }
 };
 
-//class to create a binary tree
-class Btree{
+class BStree{
      public:
-     node *root;       
-   
-    Btree()
+     node *root;
+
+    BStree()
     {
     root=NULL;
     }
 
-// to insert elements
+
  void insert(int x){
    node *temp=new node;
    temp->data=x;
    temp->parent=temp->left=temp->right=NULL;
-   
+
    if(root==NULL)
    { root=temp;
    }
-   else{ node *curr; 
+   else{ node *curr;
          curr=root;
         while(1){
             if(x<curr->data){
-              
+
                if(curr->left==NULL){
                   temp->parent=curr;
                   curr->left=temp;
                break;
-               }              
+               }
               else{curr=curr->left;}
             }
-           else { 
+           else {
                     if(curr->right==NULL)
                        {
                        temp->parent=curr;
@@ -52,27 +51,22 @@ class Btree{
                      break;}
                    else{curr=curr->right;}
                   }
-                          
+
         }
      }
  }
 
-//to display the elements
-
  void display(node *temp){
-     
+
       if(temp==NULL)
          return;
       display(temp->left);
       cout<<"\t"<<temp->data;
-      display(temp->right); 
-      cout<<"\t"<<temp->data;
+      display(temp->right);
 }
-   
-     //to search a node
+
 node* searchNode(int dt)
 {
- int count =0;
  node * check = root;
  if(check == NULL)
      return NULL;
@@ -81,130 +75,110 @@ node* searchNode(int dt)
   {
    if(check->data == dt)
    {
-  cout << "\nNode found at depth " << count <<".\n";
-  display(check);
-  return check;
+     cout << "\nNode found.\n";
+     return check;
    }
    else if(check->data > dt)
    {
-    if(check->left == NULL)  
-  {
-   cout << "\nNOT FOUND.\n";
-   return NULL;
-  }
+    if(check->left == NULL)
+      {
+       cout << "\nNOT FOUND.\n";
+       return NULL;
+      }
     else
-  check = check->left;
+       check = check->left;
    }
    else
    {
     if(check->right == NULL)
-  {
-    cout << "\nNOT FOUND.\n";
-   return NULL;
-  }
+    {
+      cout << "\nNOT FOUND.\n";
+      return NULL;
+    }
     else
-  check = check->right;
+      check = check->right;
    }
-   
-   count++;
+
   }
+
 }
-  
-  
-  //To delete a node
-void deleteNode(node*temp,int s)
+
+
+void deleteNode(int s)
 {
-  
-  if(searchNode(s)!=NULL)
-  {  
+
+  if(searchNode(s)==NULL)
+  {cout<<"Node not found \n";
+  }
+  else
+{
+
     node*curr=searchNode(s);
-    cout<<curr->data;
+
     // if this is leaf node
     if(curr->left==NULL && curr->right==NULL)
-    { 
+    { if(curr==root)
+        {root=NULL;
+        cout<<"Tree deleted \n";}
       if(curr->parent->left==curr)
         curr->parent->left=NULL;
       else
          curr->parent->right=NULL;
+     cout<<"Node deleted \n";
     }
-    // if node has only one child
+    //if it has one single child
     else if(curr->left==NULL || curr->right==NULL)
-    { 
-      if(curr!=root)
-      {
-        node* p;
-        node* c;
-        p=curr->parent; 
-        if(curr->left==NULL)
-        c=curr->right;
-        else
-        c=curr->left;
-        if(p->data>=curr->data)
-        {
-          p->left=c;
-          c->parent=p;
+    {if(curr==root)
+      {if(curr->left==NULL)
+        {root=curr->right;
+         curr->right->parent=NULL;
         }
-        else
-        {
-         p->right=c;
-         c->parent=p;
-        }
-       }
        else
-       {
-         if(curr->left==NULL)
-          root=curr->right;
-         else
-          root=curr->left;
-       }
-    }
-    // node has at most 2 child
-    else
-    {
-      node*l;
-      l=curr->left;
-      while(l->right!=NULL)
-      {
-        l=l->right;
+        {root=curr->left;
+         curr->left->parent==NULL;
+        }
+        cout<<"Node deleted \n";
       }
-      if(l->left==NULL)
-      {
-       curr->data=l->data;
-       node* p=l->parent;
-       if(p->left==l)
-        p->left=NULL;
-       else
-        p->right=NULL;
+     if(curr->left==NULL)
+      { curr->right->parent=curr->parent;
+        if(curr->parent->left==curr)
+          curr->parent->left=curr->right;
+        else
+          curr->parent->right=curr->right;
       }
       else
-      {
-        curr->data=l->data;
-        node* p=l->parent;
-        if(p->left==l)
-        p->left=l->left;
+      { curr->left->parent=curr->parent;
+        if(curr->parent->left==curr)
+           curr->parent->left=curr->left;
         else
-        p->right=l->left;
+           curr->parent->right=curr->left;
       }
+      cout<<"Node deleted \n";
     }
-  }
-}
+   //if it has two children
+   else
+   {node* temp=curr->right;
+    while(temp->left!=NULL)
+     {temp=temp->left;}
+     //temp has the element we want to replace
+     int cons;
+     cons=temp->data;
+     deleteNode(cons);
+     curr->data=cons;
 
-  
+ }}  }
 
-    
+ };
 
-};     
-
-
-int main(){
- Btree b;
+ int main(){
+ BStree b;
  cout<<"\nMENU";
  cout<<"\n1.INSERT";
  cout<<"\n2.DISPLAY";
- cout<<"\n3.SEARCH";  
+ cout<<"\n3.SEARCH";
  cout<<"\n4.DELETE";
  cout<<"\n5.EXIT";
- 
+
 int ch,x;
 do{
   cout<<"\nenter your choice (1-4): ";
@@ -216,13 +190,12 @@ do{
   case 2:b.display(b.root);break;
   case 3:cout<<"\nenter the data you want to search :";
          cin>>x;
-         b.searchNode(x);break;  
+         b.searchNode(x);break;
 case 4:cout<<"\nenter the data you want to delete :";
          cin>>x;
-         b.deleteNode(b.root,x);break;
+         b.deleteNode(x);break;
   deafult:cout<<"\ntype 1,2,3 or 4 ";break;
   }
 }while(ch!=5);
 return 0;
 }
-
